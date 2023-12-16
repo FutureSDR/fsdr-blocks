7ec9b5024b61969e538d54e6fc4f390b5d95f9b5
Loïc Fejoz <loic@fejoz.net>
2023-12-16 18:16:35 +0100

    chore(sigmf): Tests annotations

--- tests/sigmf/sigmf.rs
+++ tests/sigmf/sigmf.rs
@@ -7,7 +7,10 @@ use futuresdr::{
     macros::connect,
     runtime::{Flowgraph, Runtime},
 };
-use sigmf::{DatasetFormat, DescriptionBuilder};
+
+use futuresdr::futures::io::BufReader;
+use futuresdr::futures::io::Cursor;
+use sigmf::{Annotation, DatasetFormat, DescriptionBuilder};
 
 /// Write the data into a SigMF file,
 /// then read it back again
@@ -71,4 +74,68 @@ fn sigmf_write_read_ri8() -> Result<()> {
     let data = vec![6i8, 8, -10, 0, 12];
     let datatype = DatasetFormat::RI8;
     sigmf_write_read(datatype, data)
+}
+
+#[test]
+fn sigmf_read_write_annotation() -> Result<()> {
+    let data = vec![6u8; 45];
+    let datatype = DatasetFormat::RU8;
+    let mut fg = Flowgraph::new();
+
+    let mut desc = DescriptionBuilder::from(datatype);
+    let annot = Annotation {
+        label: Some("abc".to_string()),
+        comment: Some("the comment".to_string()),
+        sample_start: Some(10),
+        sample_count: Some(20),
+        ..Annotation::default()
+    };
+    desc.add_annotation(annot)?;
+    let annot = Annotation {
+        label: Some("the annot".to_string()),
+        comment: Some("another comment".to_string()),
+        sample_start: Some(15),
+        sample_count: Some(25),
+        ..Annotation::default()
+    };
+    desc.add_annotation(annot)?;
+    let desc = desc.build()?;
+
+    let actual_file = Cursor::new(Vec::from(data));
+    let actual_file = BufReader::new(actual_file);
+    let src1 = futuresdr::futures::executor::block_on(
+        SigMFSourceBuilder::with_data_and_description(actual_file, desc).build::<u8>(),
+    )?;
+
+    
+    let data_file_content: Vec<u8> = vec![];
+    let meta_file_content: Vec<u8> = vec![];
+    let data_file = std::io::Cursor::new(data_file_content);
+    let meta_file = std::io::Cursor::new(meta_file_content);
+    let tgt_desc = DescriptionBuilder::from(datatype);
+    let snk1 = SigMFSink::<u8, _, _>::new(data_file, tgt_desc, meta_file);
+
+    // Direct from source to sinkflowgraph
+    connect!(fg,
+        src1 > snk1;
+    );
+    // Now run the flowgraph
+    fg = Runtime::new().run(fg)?;
+
+    // Time to verify
+    let snk1 = fg
+        .kernel::<SigMFSink<u8, std::io::Cursor<Vec<u8>>, std::io::Cursor<Vec<u8>>>>(snk1)
+        .unwrap();
+    let tgt_desc = snk1.description.build()?;
+    let annotations = tgt_desc.annotations()?;
+    assert_eq!(2, annotations.len()); 
+    let annot1 = annotations.get(0).expect("the annotation should have been recreated");
+    assert_eq!("the comment", annot1.comment.as_ref().expect("comment should have been copied").as_str());
+    assert_eq!("abc", annot1.label.as_ref().expect("label should have been copied").as_str());
+
+    let annot1 = annotations.get(0).expect("the annotation should have been recreated");
+    assert_eq!("the comment", annot1.comment.as_ref().expect("comment should have been copied").as_str());
+    assert_eq!("abc", annot1.label.as_ref().expect("label should have been copied").as_str());
+
+    Ok(())
 }
