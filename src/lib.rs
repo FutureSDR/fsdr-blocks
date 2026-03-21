cbbf1a9bb555979870e33d03a94523d2e86913b2
Loïc Fejoz <loic@fejoz.net>
2026-03-21 22:51:28 +0100

    fix(ci): use min_specialization and allow incomplete_features
    - Switch from specialization to min_specialization for better stability.
    - Add #[allow(incomplete_features)] to src/lib.rs to satisfy clippy in CI/CD.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![cfg_attr(feature = "simd", feature(portable_simd))]
-#![cfg_attr(feature = "simd", feature(specialization))]
+#![cfg_attr(feature = "simd", feature(min_specialization))]
 //! This library acts as a toolbox on top of [`futuresdr`] to easily build your own flowgraph.
 //! It is made by the community for the community.
 
